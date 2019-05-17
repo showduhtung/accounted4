03aa6b27afea31ad35467d7ac03f31cf08bd5992
showduhtung <shaun.tung@gmail.com>
2019-05-17 00:21:53 -0400

    try to netlify

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ApolloProvider, Query } from 'react-apollo';
 import ApolloClient, { gql } from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql/',
+  uri: 'https://accounted-for.herokuapp.com/graphql/',
   fetchOptions: {
     credentials: 'include',
   },
