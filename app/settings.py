0fca809f0dbb1e7e1c8154f01ceac7c8b6d2e1e5
showduhtung <shaun.tung@gmail.com>
2019-05-16 23:33:34 -0400

    added http

--- app/settings.py
+++ app/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
+    'http://localhost:3000',
 )
 
 AUTHENTICATION_BACKENDS = [
@@ -140,4 +140,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
